26ae565f4a54c951b046236ed77b111210d83a94
markTward <mark.t.ward@gmail.com>
2016-10-06 13:55:39 -0700

    removal of golang book example. preparation for db lookup using query string

--- examples/db1/client/main.go
+++ examples/db1/client/main.go
@@ -11,31 +11,22 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"sync"
 )
-
-var mu sync.Mutex
-var count int
 
 func main() {
 	http.HandleFunc("/", handler)
-	http.HandleFunc("/count", counter)
+	http.HandleFunc("/query", query)
 	log.Fatal(http.ListenAndServe("localhost:8000", nil))
 }
 
 // handler echoes the Path component of the requested URL.
 func handler(w http.ResponseWriter, r *http.Request) {
-	mu.Lock()
-	count++
-	mu.Unlock()
 	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
 }
 
 // counter echoes the number of calls so far.
-func counter(w http.ResponseWriter, r *http.Request) {
-	mu.Lock()
-	fmt.Fprintf(w, "Count %d\n", count)
-	mu.Unlock()
+func query(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Query %v\tString %v\n", r.URL.Path, r.URL.Query())
 }
 
 //!-
