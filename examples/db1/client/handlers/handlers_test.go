209443c52067ec7989e178d63eff553a3c5f779b
markTward <mark.t.ward@gmail.com>
2016-10-12 18:02:58 -0700

    healthcheck handler test using net/http/httptest

--- examples/db1/client/handlers/handlers_test.go
+++ examples/db1/client/handlers/handlers_test.go
@@ -1,14 +1,44 @@
 package handlers
 
-import "testing"
+import (
+	"log"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
 
 func TestSanity(t *testing.T) {
-	if false {
-		t.Error("nothing to see here")
-	}
-}
-func TestInSanity(t *testing.T) {
+
 	if true {
-		t.Error("nothing to see here")
+		log.Println("sanity! nothing to see here ...")
+	} else {
+		t.Error("big trouble!")
+	}
+
+}
+
+func TestHealthCheck(t *testing.T) {
+	// thanks to https://elithrar.github.io/article/testing-http-handlers-go/ for tutorial
+
+	// create request for handler
+	req, err := http.NewRequest("GET", "/healthcheck", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// ResponseRecorder satisfies ResponseWriter
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(HealthCheck)
+
+	// Pass request to mock server
+	handler.ServeHTTP(rr, req)
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned incorrect response code.  expected %v received %v", status, http.StatusOK)
+	}
+
+	// test response against expected value
+	expected := `{"is_alive": "true"}`
+	if rr.Body.String() != expected {
+		t.Errorf("handler return unexpected body: expected %v received %v", expected, rr.Body.String())
 	}
 }
