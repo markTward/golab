62d775bb4f12fb221a1ecb367dac1e69bd5d4125
markTward <mark.t.ward@gmail.com>
2017-03-29 14:35:29 -0700

    rework google grpc example with web server front end

---
+++ tutorials/google.golang.org/examples/hello/greeter_client/handlers/handlers.go
@@ -0,0 +1,53 @@
+package handlers
+
+import (
+	"log"
+	"net/http"
+	"strings"
+
+	pb "github.com/markTward/gospace/tutorials/google.golang.org/examples/hello/helloworld"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+)
+
+const (
+	addressDB   = "localhost:8000"
+	defaultName = "World!"
+)
+
+var tokens = make(chan struct{}, 100)
+
+// Respond to HelloWorld request
+func HelloWorld(w http.ResponseWriter, r *http.Request) {
+	// Set up a connection to the server.
+	conn, err := grpc.Dial(addressDB, grpc.WithInsecure())
+	if err != nil {
+		log.Printf("did not connect: %v", err)
+	}
+	defer conn.Close()
+
+	// Greeter Client
+	c := pb.NewGreeterClient(conn)
+
+	// Contact the server and print out its response.
+	q := r.URL.Query()
+
+	qname, ok := q["name"]
+	var name string
+	if ok {
+		name = strings.Join(qname, ", ")
+		// for _, v := range qname {
+		// 	name = name + ", " + v
+		// }
+	} else {
+		name = defaultName
+	}
+
+	rpc, err := c.SayHello(context.Background(), &pb.HelloRequest{Name: name})
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	}
+	log.Printf("Greeting: %s", rpc.Message)
+
+	w.Write([]byte(rpc.Message))
+}
