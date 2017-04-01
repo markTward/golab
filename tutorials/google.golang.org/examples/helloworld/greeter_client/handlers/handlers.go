6bb3e09e22677199b3c7d26e25d15208134c042a
markTward <mark.t.ward@gmail.com>
2017-04-01 10:30:43 -0700

    error handling when grpc server unavailable

--- tutorials/google.golang.org/examples/helloworld/greeter_client/handlers/handlers.go
+++ tutorials/google.golang.org/examples/helloworld/greeter_client/handlers/handlers.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 	"strings"
+	"time"
 
 	pb "github.com/markTward/gospace/tutorials/google.golang.org/examples/helloworld/helloworld"
 	"golang.org/x/net/context"
@@ -12,7 +13,8 @@ import (
 )
 
 const (
-	addressDB   = "greeter-grpc:8000"
+	//	addressDB   = "greeter-grpc:8000"
+	addressDB   = ":8000"
 	defaultName = "World!"
 )
 
@@ -21,40 +23,44 @@ var tokens = make(chan struct{}, 100)
 // HealthCheck simple
 func HealthCheck(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
-	fmt.Fprint(w, "")
+	fmt.Fprint(w, "OK")
 	log.Println(r.URL.Path, http.StatusOK)
 }
 
 // HelloWorld grpc request
 func HelloWorld(w http.ResponseWriter, r *http.Request) {
 	// Set up a connection to the server.
-	conn, err := grpc.Dial(addressDB, grpc.WithInsecure())
+	conn, err := grpc.Dial(addressDB, grpc.WithInsecure(), grpc.WithBlock(), grpc.WithTimeout(5*time.Second))
+
+	// grpc server unreachable
 	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
 		log.Printf("did not connect: %v", err)
+		fmt.Fprint(w, "did not connect: %v", err)
+		return
 	}
 	defer conn.Close()
 
 	// Greeter Client
 	c := pb.NewGreeterClient(conn)
 
-	// Contact the server and print out its response.
+	// query string
 	q := r.URL.Query()
 
 	qname, ok := q["name"]
 	var name string
 	if ok {
 		name = strings.Join(qname, ", ")
-		// for _, v := range qname {
-		// 	name = name + ", " + v
-		// }
 	} else {
 		name = defaultName
 	}
 
 	rpc, err := c.SayHello(context.Background(), &pb.HelloRequest{Name: name})
 	if err != nil {
-		log.Fatalf("could not greet: %v", err)
+		log.Printf("%v", err)
+		fmt.Fprint(w, err)
+	} else {
+		log.Printf("%s?%s; grpc Message:%s", r.URL.Path, r.URL.RawQuery, rpc.Message)
+		fmt.Fprint(w, rpc.Message)
 	}
-	log.Printf("%s?%s; grpc Message:%s", r.URL.Path, r.URL.RawQuery, rpc.Message)
-	fmt.Fprint(w, rpc.Message)
 }
